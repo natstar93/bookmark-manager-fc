01e143194e808831334e590d628c0963bb5c4578
Natalie Akam <natalie.akam@gmail.com>
2015-06-30 16:37:55 +0100

    Fixed setup issue

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, ENV['postgres://fbgrvatjkdqndw:tM26wogqPAKLs3cyba8L2wicy9@ec2-54-225-130-164.compute-1.amazonaws.com:5432/d2g3gk55ff9t5d'] || "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/models/link'
 
