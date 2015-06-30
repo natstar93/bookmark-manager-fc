af0d7a292b7bba58a131c1c476e3c61865cebb9d
Natalie Akam <natalie.akam@gmail.com>
2015-06-30 16:19:44 +0100

    Added prod database link

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,9 +1,8 @@
 require 'data_mapper'
-require 'dm-postgres-adapter'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['postgres://fbgrvatjkdqndw:tM26wogqPAKLs3cyba8L2wicy9@ec2-54-225-130-164.compute-1.amazonaws.com:5432/d2g3gk55ff9t5d'] || "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/models/link'
 
