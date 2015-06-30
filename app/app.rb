e073f72d067f9158e05124e4d0541561f8065a59
Natalie Akam <natalie.akam@gmail.com>
2015-06-30 12:04:07 +0100

    Added index page (links)

--- app/app.rb
+++ app/app.rb
@@ -2,4 +2,8 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/links' do
+  	@links = Link.all
+  	erb :index
+  end
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -5,7 +5,7 @@ feature 'Viewing links' do
     expect(page.status_code).to eq 200
 
 
-    within 'url#links' do
+    within 'ul#links' do
       expect(page).to have_content('Makers Academy')
     end
   end
