e86a37d867462faa99e6e30cf1f42d9cd64e9665
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-30 12:22:00 +0100

    can now add new links (tested)

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
   get '/links' do
-  	@links = Link.all
-  	erb :index
+    @links = Link.all
+    erb :index
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
+  end
+
+  get '/links/new' do
+    erb :new
   end
 end
